cbd742fe92c6517e1aa4b557b37b5c9abcaf1fcc
Lavictus <markbaaij@lavictus.nl>
2020-04-01 16:28:26 +0200

    Added description

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 DeAuth
+
+This project started of as a deathentication project, hence the name, that proved more difficult then I expected.
+Instead I chose to make a Wifi "spammer", which uses the ESP to generate fake acces points.
+This shows how easy it is to make something that looks like an acces point, without it actually being one. 
+Real life uses for this could be to let someone connect to you instead of the acces point they think they are connecting too,
+to steal information. 
+
+~Mark
--- scan/main/scan.c
+++ scan/main/scan.c
@@ -1,5 +1,19 @@
 
 
+/*
+
+DeAuth
+
+This project started of as a deathentication project, hence the name, that proved more difficult then I expected.
+Instead I chose to make a Wifi "spammer", which uses the ESP to generate fake acces points.
+This shows how easy it is to make something that looks like an acces point, without it actually being one.
+Real life uses for this could be to let someone connect to you instead of the acces point they think they are connecting too,
+to steal information.
+
+~Mark
+
+*/
+
 // This project was made by Mark Baaij for Avans Hogeschool 
 // It uses a ESP32 with built in wifi chip to send out fake Acces Point beacon frames. 
 // This project started with the scan class in the ESP folder, but since that one did not work,
